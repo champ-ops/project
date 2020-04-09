f348a0928c70c5d26425596d194f20cda817c48b
champ-ops <beksuktanandashev@gmail.com>
2020-04-09 15:28:44 +0600

    add js for gh-pages

--- script.js
+++
@@ -1,9 +0,0 @@
-$('.burger').on('click', () => {
-    $('.burger').toggleClass('show-menu');
-    $('.top-menu').toggleClass('show')
-});
-
-$('.top-menu a').on('click', () => {
-    $('.burger').removeClass('show-menu');
-    $('.top-menu').removeClass('show')
-});
